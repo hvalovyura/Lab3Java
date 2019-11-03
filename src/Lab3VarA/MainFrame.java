0356eef6a29a43fc5a8c7f2702ba3a1b6f45ef8d
Yura <hvalovyura@gmail.com>
2019-11-04 00:51:52 +0300

    Data fields of MainFrame class was added

--- src/Lab3VarA/MainFrame.java
+++ src/Lab3VarA/MainFrame.java
@@ -1,11 +1,29 @@
 package Lab3VarA;
 
-public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame
+{
+    static private final int WIDTH = 700;
+    static private final int HEIGHT = 700;
+
+    private Double[] coefficients;
+
+    private JFileChooser fileChooser = null;
+
+    private JMenuItem saveToTextMenuItem;
+    private JMenuItem saveToGraphicMenuItem;
+    private JMenuItem searchValueMenuItem;
+
+    private JTextField textFieldFrom;
+    private JTextField textFieldTo;
+    private JTextField textFieldStep;
+    private Box hboxResult;
+
+    private GornerTableCellRenderer renderer = new GornerTableCellRenderer();
+
+    private GornerTableModel data;
 
 
 
-    public static void main(String[] args)
-    {
-        System.out.println("QQ");
-    }
 }
