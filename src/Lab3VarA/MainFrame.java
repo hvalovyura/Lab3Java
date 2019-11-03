ce277c118e2df20cdb37f9ceff29e4aef60c4d7d
Yura <hvalovyura@gmail.com>
2019-11-04 01:06:24 +0300

    menuBar is added

--- src/Lab3VarA/MainFrame.java
+++ src/Lab3VarA/MainFrame.java
@@ -1,8 +1,9 @@
 package Lab3VarA;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class MainFrame
+public class MainFrame extends JFrame
 {
     static private final int WIDTH = 700;
     static private final int HEIGHT = 700;
@@ -23,6 +24,22 @@ public class MainFrame
     private GornerTableCellRenderer renderer = new GornerTableCellRenderer();
 
     private GornerTableModel data;
+
+    public MainFrame(Double[] coefficients)
+    {
+        super("Табулирование многочлена на отрезке по схеме Горнера");
+        this.coefficients = coefficients;
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH)/2, (kit.getScreenSize().height - HEIGHT)/2);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+        JMenu tableMenu = new JMenu("Таблица");
+        menuBar.add(tableMenu);
+    }
 
 
 
