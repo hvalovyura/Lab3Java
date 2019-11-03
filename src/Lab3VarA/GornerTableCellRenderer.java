89dcb2d0701845ca14dc60971a8ae501ea45e2b7
Yura <hvalovyura@gmail.com>
2019-11-04 00:06:37 +0300

    GornerTableCellRenderer is added

---
+++ src/Lab3VarA/GornerTableCellRenderer.java
@@ -0,0 +1,46 @@
+package Lab3VarA;
+
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+
+public class GornerTableCellRenderer implements TableCellRenderer
+{
+    private String needle = null;
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+    private DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
+
+    public GornerTableCellRenderer()
+    {
+        panel.add(label);
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        formatter.setMaximumFractionDigits(5);
+        formatter.setGroupingUsed(false);
+        DecimalFormatSymbols dottedDouble = formatter.getDecimalFormatSymbols();
+        dottedDouble.setDecimalSeparator('.');
+        formatter.setDecimalFormatSymbols(dottedDouble);
+    }
+
+    public void setNeedle(String needle) {
+        this.needle = needle;
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        String formattedDouble = formatter.format(value);
+        label.setText(formattedDouble);
+        if (column == 1 && needle != null && needle.equals(value))
+        {
+            panel.setBackground(Color.RED);
+        }
+        else
+        {
+            panel.setBackground(Color.WHITE);
+        }
+        return panel;
+    }
+}
